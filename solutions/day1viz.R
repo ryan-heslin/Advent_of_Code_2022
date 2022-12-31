8bb4f2cc74f884e0ea3799f8f8a0325295e49a29
ryan-heslin <rwheslin@gmail.com>
2022-12-31 08:32:57 -0500

    Remove old file

--- solutions/day1viz.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-
-calories <- readLines("inputs/day1.txt")
-calories <- data.frame(calories = calories)
-calories$elf <- cumsum(calories$calories == "") + 1
-calories <- calories[calories$calories != "", ]
